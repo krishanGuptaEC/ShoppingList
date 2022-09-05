d064871d5a48e59fe3b944dc1023ec503fed8980
krishan Gupta <krishangupta.ece@gmail.com>
2022-09-06 04:35:38 +0530

    first commit

---
+++ src/main/java/com/SMK/SMKSHOPS/SmkshopsApplication.java
@@ -0,0 +1,13 @@
+package com.SMK.SMKSHOPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmkshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmkshopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SMK/SMKSHOPS/SmkshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SMK.SMKSHOPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmkshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
